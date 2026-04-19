74bd1b77d8220e14c93a4c8d1429529b027277b3
galoihhbg <101344155+galoihhbg@users.noreply.github.com>
2026-04-20 02:06:39 +0700

    feat: define Comic model and remove accidental dev executable

--- internal/pkg/model/comic.go
+++ internal/pkg/model/comic.go
@@ -1,11 +1,14 @@
 package model
 
 import (
+	"encoding/json"
 	"manga-go/internal/pkg/common"
 	"manga-go/internal/pkg/constant"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Comic struct {
@@ -46,4 +49,82 @@ type Comic struct {
 
 func (Comic) TableName() string {
 	return "comics"
+}
+
+type comicJSON struct {
+	ID                 uuid.UUID               `json:"id"`
+	CreatedAt          *time.Time              `json:"createdAt"`
+	UpdatedAt          *time.Time              `json:"updatedAt"`
+	DeletedAt          gorm.DeletedAt          `json:"deletedAt"`
+	Title              string                  `json:"title"`
+	Slug               string                  `json:"slug"`
+	AlternativeTitles  common.StringSlice      `json:"alternativeTitles"`
+	Description        *string                 `json:"description"`
+	Thumbnail          *string                 `json:"thumbnail"`
+	Banner             *string                 `json:"banner"`
+	Type               constant.ComicType      `json:"type"`
+	Status             constant.ComicStatus    `json:"status"`
+	AgeRating          constant.ComicAgeRating `json:"ageRating"`
+	IsPublished        bool                    `json:"isPublished"`
+	IsHot              bool                    `json:"isHot"`
+	IsFeatured         bool                    `json:"isFeatured"`
+	PublishedYear      *int                    `json:"publishedYear"`
+	LastChapterAt      *time.Time              `json:"lastChapterAt"`
+	TranslationGroupID *uuid.UUID              `json:"translationGroupId,omitempty"`
+	UploadedByID       *uuid.UUID              `json:"uploadedById,omitempty"`
+	FollowCount        int                     `json:"followCount"`
+	RatingCount        int                     `json:"ratingCount"`
+	ChapterCount       int                     `json:"chapterCount"`
+	AvgRating          *float64                `json:"avgRating"`
+	TranslationGroup   *TranslationGroup       `json:"translationGroup,omitempty"`
+	UploadedBy         *User                   `json:"uploadedBy,omitempty"`
+	Artists            []*Author               `json:"artists"`
+	Authors            []*Author               `json:"authors"`
+	Genres             []*Genre                `json:"genres"`
+	Tags               []*Tag                  `json:"tags"`
+	Chapters           []*Chapter              `json:"chapters,omitempty"`
+}
+
+func addFileContentPrefix(s *string) *string {
+	if s == nil || *s == "" || strings.HasPrefix(*s, "/") || strings.HasPrefix(*s, "http") {
+		return s
+	}
+	v := "/files/content/" + *s
+	return &v
+}
+
+func (c Comic) MarshalJSON() ([]byte, error) {
+	return json.Marshal(comicJSON{
+		ID:                 c.ID,
+		CreatedAt:          c.CreatedAt,
+		UpdatedAt:          c.UpdatedAt,
+		DeletedAt:          c.DeletedAt,
+		Title:              c.Title,
+		Slug:               c.Slug,
+		AlternativeTitles:  c.AlternativeTitles,
+		Description:        c.Description,
+		Thumbnail:          addFileContentPrefix(c.Thumbnail),
+		Banner:             addFileContentPrefix(c.Banner),
+		Type:               c.Type,
+		Status:             c.Status,
+		AgeRating:          c.AgeRating,
+		IsPublished:        c.IsPublished,
+		IsHot:              c.IsHot,
+		IsFeatured:         c.IsFeatured,
+		PublishedYear:      c.PublishedYear,
+		LastChapterAt:      c.LastChapterAt,
+		TranslationGroupID: c.TranslationGroupID,
+		UploadedByID:       c.UploadedByID,
+		FollowCount:        c.FollowCount,
+		RatingCount:        c.RatingCount,
+		ChapterCount:       c.ChapterCount,
+		AvgRating:          c.AvgRating,
+		TranslationGroup:   c.TranslationGroup,
+		UploadedBy:         c.UploadedBy,
+		Artists:            c.Artists,
+		Authors:            c.Authors,
+		Genres:             c.Genres,
+		Tags:               c.Tags,
+		Chapters:           c.Chapters,
+	})
 }
