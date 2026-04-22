115071bad6878a64cd9f7d5c2c1e73430df5a716
kuon bui <cuongbuimanh1203@gmail.com>
2026-04-22 14:18:01 +0700

    feat: implement reading history creation and update logic

--- internal/pkg/services/reading_history/create.go
+++ internal/pkg/services/reading_history/create.go
@@ -2,6 +2,7 @@ package readinghistoryservice
 
 import (
 	"context"
+	"errors"
 	"manga-go/internal/app/api/common/response"
 	"manga-go/internal/pkg/model"
 	readinghistoryrequest "manga-go/internal/pkg/request/reading_history"
@@ -9,10 +10,35 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 func (s *ReadingHistoryService) CreateReadingHistory(ctx context.Context, userID uuid.UUID, req *readinghistoryrequest.CreateReadingHistoryRequest) response.Result {
 	now := time.Now()
+	conditions := []any{
+		clause.Eq{Column: "user_id", Value: userID},
+		clause.Eq{Column: "chapter_id", Value: req.ChapterID},
+	}
+
+	existingReadingHistory, err := s.readingHistoryRepo.FindOne(ctx, conditions, nil)
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		s.logger.Error("Failed to find reading history", "error", err)
+		return response.ResultErrDb(err)
+	}
+
+	if existingReadingHistory != nil && existingReadingHistory.ID != uuid.Nil {
+		existingReadingHistory.LastReadAt = &now
+		if err := s.readingHistoryRepo.Update(ctx, conditions, map[string]any{
+			"last_read_at": now,
+		}); err != nil {
+			s.logger.Error("Failed to update reading history", "error", err)
+			return response.ResultErrDb(err)
+		}
+
+		return response.ResultSuccess("Reading history updated successfully", existingReadingHistory)
+	}
+
 	readingHistory := model.ReadingHistory{
 		UserID:     userID,
 		ChapterID:  req.ChapterID,
@@ -20,8 +46,8 @@ func (s *ReadingHistoryService) CreateReadingHistory(ctx context.Context, userID
 		LastReadAt: &now,
 	}
 
-	if err := s.readingHistoryRepo.Create(ctx, &readingHistory); err != nil {
-		s.logger.Error("Failed to create reading history", "error", err)
+	if err := s.readingHistoryRepo.Save(ctx, &readingHistory); err != nil {
+		s.logger.Error("Failed to save reading history", "error", err)
 		return response.ResultErrDb(err)
 	}
 
