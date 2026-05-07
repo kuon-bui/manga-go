64a96321301be0c1fdc826559d49f079bf664dba
kuon bui <cuongbuimanh1203@gmail.com>
2026-05-07 13:47:48 +0700

    feat: add user read status to chapter listing and refactor recent updates response

--- internal/pkg/services/chapter/list.go
+++ internal/pkg/services/chapter/list.go
@@ -2,9 +2,12 @@ package chapterserivce
 
 import (
 	"context"
+	"errors"
 	"manga-go/internal/app/api/common/response"
 	"manga-go/internal/pkg/common"
+	"manga-go/internal/pkg/utils"
 
+	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
 
@@ -20,6 +23,26 @@ func (s *ChapterService) ListChapters(ctx context.Context, paging *common.Paging
 	if err != nil {
 		s.logger.Error("Failed to list chapters", "error", err)
 		return response.ResultErrDb(err)
+	}
+	user, err := utils.GetCurrentUserFormContext(ctx)
+	if err != nil {
+		s.logger.Error("Failed to get current user from context", "error", err)
+		return response.ResultErrInternal(err)
+	}
+
+	userComicRead, err := s.userComicReadRepo.FindOne(ctx, []any{
+		clause.Eq{Column: "user_id", Value: user.ID},
+		clause.Eq{Column: "comic_id", Value: comicID},
+	}, nil)
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		s.logger.Error("Failed to find user comic read data", "error", err)
+		return response.ResultErrDb(err)
+	}
+
+	if userComicRead != nil {
+		for _, chapter := range chapters {
+			chapter.IsRead = userComicRead.ReadData.IsRead(int(chapter.ChapterIdx))
+		}
 	}
 
 	return response.ResultPaginationData(chapters, total, "Chapters retrieved successfully")
